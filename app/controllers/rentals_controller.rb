caf8a61c4161de645fdffc96cf22a0147e89bb56
Edgar Cardenas <edcardenash@gmail.com>
2022-12-09 13:52:28 -0300

    Creación del view de parkings, modificación controlador de rentals, cambio de rutas para vincular parkings con rentals, creación de formulario para rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,31 +1,38 @@
 class RentalsController < ApplicationController
-  before_action :set_parking, only: [:new, :create]
+  before_action :set_rental, only: [:destroy]
+  before_action :authenticate_user!, only: :create
+
   def index
     @rentals = Rental.all.where(user_id: current_user.id)
   end
 
-  def new
-    @rental = Rental.new
-  end
-
   def create
     @rental = Rental.new(rental_params)
-    @rental.parking = @parking
     @rental.user = current_user
+    @parking = Parking.find(params[:parking_id])
+    @rental.parking = @parking
+    @rental.total_amount = (@rental.end_date - @rental.start_date).to_i * @rental.parking.price_per_day.to_i
     if @rental.save
-      redirect_to parking_rentals_path(@parking)
+      flash[:notice] = '¡Su reserva ha sido procesada'
+      redirect_to user_path(current_user)
     else
-      redirect_to parking_path(@parking)
+      flash[:notice] = 'Lo sentimos, fechas nos disponibles'
+      render "parkings/show"
     end
+  end
+
+  def destroy
+    @rental.destroy
+    redirect_to user_path(current_user)
   end
 
   private
 
-  def set_parking
-    @parking = Parking.find(params[:parking_id])
+  def set_rental
+    @rental = Rental.find(params[:id])
   end
 
-  def rentals_params
-    params.require(:rental).permit(:start_date, :end_date, :total_amount, :parking_id)
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date, :total_amount, :user_id, :parking_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :parkings
+  resources :parkings do
+    resources :rentals, except: [:index, :new, :edit, :update, :destroy]
+  end
   resources :rentals
   resources :reviews
   resources :countrys
