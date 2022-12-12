afb527d10e5dbe0ab67368a63329d1a46fc0b273
Enrique Altamirano <enrique.altamiros@gmail.com>
2022-12-12 19:59:39 -0300

    mejorando la vista de publicacion de un parking

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,7 +2,7 @@ class Rental < ApplicationRecord
   has_one :parking
   belongs_to :user
   belongs_to :parking
-  # validates :start_date, presence: true, comparison: { greater_than_or_equal_to: :end_date }
+  validates :start_date, presence: true
   validates :end_date, presence: true
   validates :total_amount, presence: true, numericality: true
 end
